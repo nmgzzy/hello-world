afa785dadff3ef2ea26b08f8eb7086808f47afbb
nmgzzy <283003556@qq.com>
2016-12-07 12:07:23 +0800

    一元二次方程.c

---
+++ 一元二次方程.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	double x1,x2;
+	printf("请输入a,b,c:\n");
+	scanf("%d,%d,%d",&a,&b,&c);
+	if((sqrt(b*b-4*a*c))>0){
+		x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+		x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+		printf("x1=%7.2lf,x2=%7.2lf\n",x1,x2);
+	}
+	else if((sqrt(b*b-4*a*c))==0){
+		x1=-b/(2*a);
+		printf("x1=x2=%7.2lf\n",x1);
+	}else{
+		printf("没有实数根\n");
+	}
+
+return 0;
+}
