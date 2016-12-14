3413377ff5b539cda1affea554eb7563a758054b
nmgzzy <283003556@qq.com>
2016-12-14 02:29:00 -0600

    两数交换

---
+++ 两数交换.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+	printf("«Î ‰»Îa,b:\n");
+	scanf("%d,%d",&a,&b);
+	printf("a=%d,b=%d\n",a,b);
+	c=a;
+	a=b;
+	b=c;
+	printf("a=%d,b=%d\n",a,b);
+    return 0;
+}
