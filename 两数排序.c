9306e3627c45fa2041eca71b745183feabf39eda
nmgzzy <283003556@qq.com>
2016-12-14 02:29:23 -0600

    两数排序

---
+++ 两数排序.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,p,q;
+	printf("请输入两个不相等的数：a,b\n");
+	scanf("%d,%d",&a,&b);
+	
+	if(a<b){
+		p=a;
+		q=b;}
+	else{
+		p=b;
+		q=a;}
+	printf("%d<%d",p,q);
+	return 0;
+
+}
