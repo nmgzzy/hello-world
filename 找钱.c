b900612d111247c5b44a54430068a6f041be2d5d
nmgzzy <283003556@qq.com>
2016-12-14 02:44:29 -0600

    计算找零

---
+++ 找钱.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int amount=100;
+	int price=0;
+	printf("请输入金额：");
+	scanf("%d",&price);
+	pringf("请输入票面：");
+	scanf("%d",&amount);
+	int change=amount-price;
+	printf ("找您%d元。\n",change); 
+	return 0;
+}
