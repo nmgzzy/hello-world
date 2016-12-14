e84ff4c469c077318af49f71902114770c84a3e5
nmgzzy <283003556@qq.com>
2016-12-14 02:28:39 -0600

    斐波那契数列
    前20项

---
+++ 斐波那契.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,b=2,c,i;
+	c=a+b;
+    printf("%8d%8d",a,b);
+	for(i=1;i<=20;i++)
+    {
+    	printf("%8d",c);
+    	(a>b)?(b=c):(a=c);
+		c=a+b;
+		if(i%4==0)
+		printf("\n");
+	}
+	
+	return 0;
+}
