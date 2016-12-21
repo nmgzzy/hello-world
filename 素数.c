59284b7d63e03824a4b0a47528e5064da261f18a
nmgzzy <283003556@qq.com>
2016-12-21 11:09:52 +0800

    判断素数
    无主函数

---
+++ 素数.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int isPrime(int x)
+{
+	int ret=1;
+	int i;
+	if( x==1 || (x%2==0 && x!=2) )
+		ret=0;
+	for(i=3;i<sqrt(x);i+=2)
+	{
+		if(x%i==0)
+		{
+			ret=0;
+			break;
+		}
+	}
+	return ret;
+}
