fb610ca89e1be8d69150b1ac5f24aa2cafa02589
nmgzzy <283003556@qq.com>
2017-02-02 10:46:01 +0800

    单词长度
    输入一句话，统计每个单词的长度

---
+++ 单词长度.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,n;
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		for(n=0;str[i]!=' ';i++)
+		{
+			if(str[i]=='.')
+			break;
+			n++;
+		}
+		if(n!=0)
+		{
+			printf("%d",n);
+			if(str[i+1]!='\0')
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return 0;
+}
