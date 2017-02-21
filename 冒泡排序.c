b76d8af28b4218a08577b57ecdcd76f83cd0ee24
nmgzzy <283003556@qq.com>
2017-02-21 19:28:20 +0800

    冒泡排序

---
+++ 冒泡排序.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define SIZE 4 
+int main()
+{
+	int a[4],i,j,t;
+	for(i=0;i<SIZE;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	for(i=1;i<SIZE;i++)
+	{
+		for(j=0;j<SIZE-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+	for(i=0;i<SIZE;i++)
+	{
+	printf("%5d",a[i]);
+	}
+	printf("\n");
+	return 0;
+}
