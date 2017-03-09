15c2f4c8d6caa80ba99d9e621a6cb61780ce8c96
nmgzzy <283003556@qq.com>
2017-03-09 17:59:26 +0800

    Update 冒泡排序.c

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -5,7 +5,7 @@ int main()
 	int a[4],i,j,t;
 	for(i=0;i<SIZE;i++)
 	{
-	scanf("%d",&a[i]);
+		scanf("%d",&a[i]);
 	}
 	for(i=1;i<SIZE;i++)
 	{
@@ -13,15 +13,13 @@ int main()
 		{
 			if(a[j]>a[j+1])
 			{
-				t=a[j];
-				a[j]=a[j+1];
-				a[j+1]=t;
+				t=a[j]; a[j]=a[j+1]; a[j+1]=t;
 			}
 		}
 	}
 	for(i=0;i<SIZE;i++)
 	{
-	printf("%5d",a[i]);
+		printf("%5d",a[i]);
 	}
 	printf("\n");
 	return 0;
