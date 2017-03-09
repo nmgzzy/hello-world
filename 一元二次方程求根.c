01915a854b16221e32b82e78494d38cb8ed637cc
nmgzzy <283003556@qq.com>
2017-03-09 17:51:31 +0800

    Update 一元二次方程求根.c

--- 一元二次方程求根.c
+++ 一元二次方程求根.c
@@ -4,26 +4,20 @@
 int main()
 {
 	double a,b,c,x1,x2,m,p,q;
-
-	printf("ÇëÊäÈëa,b,c:\n");
-
-	scanf("%lf,%lf,%lf",&a,&b,&c);
-
+	printf("请输入a b c:\n");
+	scanf("%lf%lf%lf",&a,&b,&c);
 	p=(-b/(2*a));
 	q=sqrt(b*b-4*a*c)/(2*a);
-
 	if(a==0)
 	{	x1=-b/c;
 		printf("x1=x2=%7.2lf\n",x1);
-	}
-	else
+	}else
 	{
 		if((b*b-4*a*c)>1e-6)
 		{	x1=p+q;
 			x2=p-q;
 			printf("x1=%7.2lf\n",x1);
 			printf("x2=%7.2lf\n",x2);
-			x2=%7.2lf\n
 		}
 		else if(fabs((b*b-4*a*c))<=1e-6)
 		{	x1=p;
@@ -32,8 +26,7 @@ int main()
 		{	m=sqrt(-(b*b-4*a*c))/(2*a);
 			printf("x1=%7.2lf+%7.2lfi\n",p,m);
 			printf("x2=%7.2lf-%7.2lfi\n",p,m);
-			
 		}
 	}
-return 0;
+	return 0;
 }
