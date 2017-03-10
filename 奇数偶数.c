7907083f48929d50519d93274457af21fe4dcb76
nmgzzy <283003556@qq.com>
2017-03-10 14:58:31 +0800

    Update 奇数偶数.c

--- 奇数偶数.c
+++ 奇数偶数.c
@@ -5,19 +5,17 @@ int main()
 	int x=0;
 	int j=0;
 	int o=0;
-do
-{
-	scanf("%d",&x);
+	do
+	{
+		scanf("%d",&x);
 		if (x>=100000);
 		else if(x<=0);
 		else if (x%2==0) 
 			o=o+1;
 		else if (x%2!=0)
 			j=j+1;
-	
-} while (x!=-1);
-
-printf("%d ",j);
-printf("%d",o);
+	} while (x!=-1);
+	printf("%d ",j);	
+	printf("%d",o);
 	return 0;
 }
