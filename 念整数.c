f481a4f2165a1303aca9b6f00ab4cbb5e3f1430c
nmgzzy <283003556@qq.com>
2017-03-10 15:01:50 +0800

    Update 念整数.c

--- 念整数.c
+++ 念整数.c
@@ -7,7 +7,6 @@ int main(){
 		printf("fu ");
 		x=-x;
 	}
-
 	int mask=1;
 	int t=x;
 	while (t>9){
@@ -17,30 +16,28 @@ int main(){
 	do
 	{
 		int d=x/mask;
-
-	switch (d){
-		case 1:printf("yi");
-		break;
-		case 2:printf("er");
-		break;
-		case 3:printf("san");
-		break;
-		case 4:printf("si");
-		break;
-		case 5:printf("wu");
-		break;
-		case 6:printf("liu");
-		break;
-		case 7:printf("qi");
-		break;
-		case 8:printf("ba");
-		break;
-		case 9:printf("jiu");
-		break;
-		case 0:printf("ling");
-		break;
-	}
-	
+		switch (d){
+			case 1:printf("yi");
+			break;
+			case 2:printf("er");
+			break;
+			case 3:printf("san");
+			break;
+			case 4:printf("si");
+			break;
+			case 5:printf("wu");
+			break;
+			case 6:printf("liu");
+			break;
+			case 7:printf("qi");
+			break;
+			case 8:printf("ba");
+			break;
+			case 9:printf("jiu");
+			break;
+			case 0:printf("ling");
+			break;
+		}
 		if (mask>9)
 		{
 			printf(" ");
@@ -48,7 +45,5 @@ int main(){
 		x%=mask;
 		mask/=10;
 	} while (mask>0);
-
 	return 0;
 }
-
